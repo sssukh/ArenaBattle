0d11e0d959e8fd9bab7c55b0484b4714f6af138a
sssukh <sssukh50@gmail.com>
2024-03-14 10:33:39 +0900

    Set Character and Control

---
+++ Source/ArenaBattle/Character/ABCharacterPlayer.h
@@ -0,0 +1,67 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/ABCharacterBase.h"
+#include "InputActionValue.h"
+#include "ABCharacterPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARENABATTLE_API AABCharacterPlayer : public AABCharacterBase
+{
+	GENERATED_BODY()
+public:
+	AABCharacterPlayer();
+
+protected:
+	// 입력 매핑 컨텍스트를 할당하는 역할
+	virtual void BeginPlay() override;
+
+public:
+	// 액션과 Move, Jump를 매핑
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)	override;
+
+// Character Control Section
+protected:
+	void ChangeCharacterControl();
+	void SetCharacterControl(ECharacterControlType NewCharacterControlType);
+	virtual void SetCharacterControlData(const class UABCharacterControlData* CharacterControlData) override;
+
+// Camera Section
+protected:
+	// Meta의 다음 내용은 Private으로 선언된 언리얼 오브젝트의 객체들을 블루프린트에서도 접근할 수 있도록 하는 지시자이다.
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly, Category = Camera, Meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class USpringArmComponent> CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, Meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UCameraComponent> FollowCamera;
+
+
+// Input Section
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, Meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UInputAction> JumpAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, Meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UInputAction> ChangeControlAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, Meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UInputAction> ShoulderMoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, Meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UInputAction> QuaterMoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, Meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UInputAction> ShoulderLookAction;
+
+	void ShoulderMove(const FInputActionValue& Value);
+	void ShoulderLook(const FInputActionValue& Value);
+
+	void QuaterMove(const FInputActionValue& Value);
+
+	ECharacterControlType CurrentCharacterControlType;
+};
